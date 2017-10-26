c525010ddbd17f4e124aef5c09d9659c368c8326
Juanmi Alberola <jmalberola@florida-uni.es>
2017-10-26 08:39:41 +0200

    commit inicialÇ

---
+++ src/dir/Principal.java
@@ -0,0 +1,19 @@
+package dir;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public static void localizar(){
+		
+	}
+	
+	public static void comparar(){
+		
+	}
+
+}
