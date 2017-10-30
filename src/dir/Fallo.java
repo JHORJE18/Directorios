c360995f4bd7f62cf5dd100ebd1f32def5109b0a
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-10-30 20:10:30 +0100

    #EJ3
    Ejercicio 3
    Muestra si existe o no un fichero / carpeta en una ubicación determianda con Excepciones en el main

---
+++ src/dir/Fallo.java
@@ -0,0 +1,13 @@
+package dir;
+
+		/*
+		 *  #EJ3 Excepcion propia
+		 */
+
+public class Fallo extends Exception {
+
+	public Fallo(String msg) {
+		super(msg);
+    }
+	
+}
--- src/dir/Principal.java
+++ src/dir/Principal.java
@@ -1,15 +1,40 @@
 package dir;
 
+import java.io.File;
+
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
+		File dir = new File("prueba/carpeta");
+		String file = "hola.txt";
 
+		try {
+			localizar(dir, file);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
 	}
 	
 	
-	public static void localizar(){
+	public static int localizar(File carpeta, String archivo) throws Exception {
+		//Comprobamos que es una carpeta
+		if (!carpeta.isDirectory()) {
+			throw new Exception("El supuesto directorio \"" + carpeta.getAbsolutePath() + "\" no es un directorio");
+		}
 		
+		//Construimos el archivo
+		File archivoTEMP = new File(carpeta + "/" + archivo);
+		
+		//Comprobamos que el archivo existe
+		if (!archivoTEMP.exists()) {
+			throw new Exception("El archivo \"" + archivo + "\" no existe");
+		} else {
+			System.out.println("Correcto! \n" + "Se ha encontrado un fichero llamado \"" + archivo + "\" en el directorio \n\"" + carpeta.getAbsolutePath() + "\"");
+		}
+						
+		return 1;
 	}
 	
 	public static void comparar(){
